3c456683331a91b0c256f9454d171cd48da9e9e0
nebolisa25 <kennethnebolisa@gmail.com>
2025-08-08 11:37:23 +0100

    Initial Commit

---
+++ RGBGuessGame/RGBGuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBGuessGameApp.swift
+//  RGBGuessGame
+//
+//  Created by Kene on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RGBGuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RGBGuessGameTests/RGBGuessGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  RGBGuessGameTests.swift
+//  RGBGuessGameTests
+//
+//  Created by Kene on 8/8/25.
+//
+
+import Testing
+@testable import RGBGuessGame
+
+struct RGBGuessGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
